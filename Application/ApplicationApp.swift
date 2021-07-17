cf686ad689c90262b7dd1bffa98f2c13ee046b14
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2021-07-16 21:49:43 -0300

    Initial Commit

---
+++ Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by Luiz Fernando Salvaterra on 16/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
